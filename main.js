05a2f376ef4302e9fe349843232a2fd37424f7c4
Ben Iofel <iofelben@gmail.com>
2016-11-12 23:53:05 -0500

    pass parent in tree-walking callback to avoid an API call

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const snoowrap = require('snoowrap')
 const r = new snoowrap(require('./config.js'))
 r.config({continueAfterRatelimitError: true})
 
-const mapComment = cb => cms => {
-    cb(cms)
-    cms.replies.forEach(mapComment(cb))
+function mapComment(comment, parent, callback) {
+    callback(comment, parent)
+    comment.replies.forEach(reply => mapComment(reply, comment, callback))
 }
 
 const sub = process.argv[2] || 'BlackPeopleTwitter' // node ./main.js [sub]
@@ -14,17 +14,17 @@ r.getSubreddit(sub).getHot().forEach(post => {
     post
         .expandReplies({limit: 1, depth: 5})
         .comments
-        .forEach(mapComment(cm => {
-            if (cm.score > 100) {
-                r.getComment(cm.parent_id).fetch().then(parent => {
+        .forEach(cmt => {
+            mapComment(cmt, null, (cm, parent) => {
+                if (cm.score > 100) {
                     // max parent score for the child to be interesting
                     // you can experiment with this formula by fitting a line
                     // to points (x = child score, y = max parent score)
                     const parentMax = cm.score * 0.4 + 20
                     if (parent instanceof snoowrap.objects.Comment && parent.score <= parentMax) {
-                        console.log(`${post.url}${cm.name.slice(3)}?context=10000`)
+                        console.log(`https://reddit.com${post.permalink}${cm.name.slice(3)}?context=10000`)
                     }
-                })
-            }
-        }))
+                }
+            })
+        })
 })
